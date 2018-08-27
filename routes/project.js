69a00b3e96b3e52ef80f7beb4d51c338b34cea75
Razvan G <razvan@codeforge.ro>
2018-08-27 12:25:52 +0300

    added route for creating new project

--- routes/project.js
+++ routes/project.js
@@ -8,8 +8,48 @@ var Project = require('../models/project')
 var User = require('../models/user')
 
 router.post('/projects', (req, res)=>{
+    req.checkBody('name', 'Missing project name').notEmpty()
+    req.checkBody('description', 'Missing project description').notEmpty()
+    req.checkBody('user_id', 'Missing user id').notEmpty()
+    req.getValidationResult().then( (result) =>{
+        if( !result.isEmpty() ){
+            throw NormalError.create('Error: Project not created')
+        }
+
+        User.where( {id: req.body.user_id} ).fetch()
+            .then( (user) =>{
+                if( !user ){
+                    throw NormalError.create('Error: could not get user')
+                }
+
+                var info = {}
+                info.name = req.body.name
+                info.description = req.body.description
+                info.user_id = req.body.user_id
+
+                return Project.forge(info).save()
+            })
+            .then( (result) =>{
+                if( !result ){
+                    throw NormalError.create('Error: could not create project')
+                }
+
+                res.send('Project Created')
+            })
+            .catch( (reason) =>{
+                if( reason.send_message )
+                {
+                    res.status(500).send({errors:reason.message})
+                    return
+                }
     
-    res.send('ok')
+                console.log('Adding project failed')
+                console.log(reason)
+                res.status(500).send({'errors':[{'msg':'Failed to create project. Try again.'}]})
+            })
+
+    })
+    
 })
 
 router.get('/projects', (req, res)=>{
