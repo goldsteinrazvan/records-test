38e5f23be5a6cd47cad83b284d6fdcae78e23ca4
Razvan G <razvan.goldstein@gmail.com>
2018-08-28 12:14:13 +0300

    added login required function

--- utils/auth_helpers.js
+++ utils/auth_helpers.js
@@ -4,4 +4,13 @@ function comparePass(userPass, databasePass){
     return bcrypt.compareSync(userPass, databasePass)
 }
 
+function loginRequired(req, res, next){
+    if ( !req.user ){
+        return res.status(401).send({"status":"Please log in"})
+    }
+
+    return next()
+}
+
 module.exports.compare = comparePass
+module.exports.loginRequired = loginRequired
