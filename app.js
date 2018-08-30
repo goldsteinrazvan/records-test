b99da26bc8979cb52c7270322757e12bc3b41cb8
Razvan G <razvan.goldstein@gmail.com>
2018-08-30 11:23:43 +0300

    mirror request origin and allow credentials header

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors({origin:true, credentials: true}))
 
 app.options('*', cors())
 
